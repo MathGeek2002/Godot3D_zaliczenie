a2febdb15108fe63e9cf4e84c747fc3936b00871
MathGeek2002 <sjakmowicz7@interia.pl>
2023-06-05 19:34:43 +0200

    fire ball

--- ZALICZENIE/StateMachine/FollowPlayerState.gd
+++ ZALICZENIE/StateMachine/FollowPlayerState.gd
@@ -6,11 +6,13 @@ export (float) var move_speed = 10
 export(float) var Acceleration = 3
 export(float) var runDistance
 export(float) var run_speed = 6
+export(float) var turn_speed = 4
 
 export (NodePath) var attack_node
 export (NodePath) var attack_combo_node
 export (NodePath) var tount_node
 export (NodePath) var magic_attack_node
+export (NodePath) var eyesPath
 
 onready var attack_state: BaseState = get_node(attack_node)
 onready var attack_combo_state: BaseState = get_node(attack_combo_node)
@@ -18,6 +20,7 @@ onready var tount_state: BaseState = get_node(tount_node)
 onready var magic_attack_state: BaseState = get_node(magic_attack_node)
 
 onready var timer = get_node(timerPath)
+onready var eyes = get_node(eyesPath)
 
 var Speed = Vector3()
 
@@ -48,11 +51,10 @@ func physics_process(delta: float) -> BaseState:
 		owner.move_speed = move_speed
 		
 	animator.set("parameters/Locomotion/blend_position", owner.move_speed / run_speed)
+
 	
-	var playerPosXZ = PlayerPosition.value
-	playerPosXZ.y = 0
-	
-	owner.look_at(playerPosXZ, Vector3.UP)
+	eyes.look_at(PlayerPosition.value, Vector3.UP)
+	owner.rotate_y(rad2deg(eyes.rotation.y * turn_speed * delta))
 	
 	return null
 
@@ -63,7 +65,12 @@ func calculatePath():
 	
 func getRandomAction():
 	
-	var attacks = [attack_state, attack_combo_state, magic_attack_state]
+	var attacks = [
+	attack_state,
+	attack_combo_state, 
+	magic_attack_state,
+	attack_combo_state
+	]
 	return attacks[randi() % attacks.size()]
 	
 func exit():
